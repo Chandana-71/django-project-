95c1d0af4d32db1e3e61424f05abb834af9a47a9
Chandana-71 <chandanakamaraj@gmail.com>
2026-03-16 23:59:17 +0530

    deployment setup

--- qa_webiste/settings.py
+++ qa_webiste/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=tg66=(36=c$&*5v)8&5*f-lv7+#9^lp-*nmif0&#l8i=qqks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
